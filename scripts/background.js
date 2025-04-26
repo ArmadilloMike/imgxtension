a63dd4d137a7590abda3d54d949ce6ed3f8efc63
32-Bit <103079138+CreeperKing77@users.noreply.github.com>
2025-04-26 10:45:04 -0500

    popup update

--- scripts/background.js
+++ scripts/background.js
@@ -1,43 +1,43 @@
-function fetchNotifications(sessionCookie) {
+function fetchData() {
   fetch("https://imgflip.com/ajax_get_le_data", {
     method: "GET",
     headers: {
       "Accept": "application/json",
       "x-requested-with": "XMLHttpRequest",
-      "Cookie": `iflipsess=${sessionCookie}`, // Manually attach the cookie
     },
   })
     .then(response => response.json())
     .then(data => {
       console.log("User Data:", data);
       const notificationCount = data.user && data.user.nots !== undefined ? String(data.user.nots) : "";
-      //fixme
-      chrome.action.setBadgeText({ text: notificationCount });
-      chrome.action.setBadgeBackgroundColor({ color: "#D72E62" })
-      console.log(`set notif count to ${notificationCount}`)
+      if (data.user.id !== 0) {
+        //set badge to notifs
+        chrome.action.setBadgeText({ text: notificationCount });
+        chrome.action.setBadgeBackgroundColor({ color: "#D72E62" })
+        console.log(`set notif count to ${notificationCount}`)
+        chrome.runtime.sendMessage({ type: "load_data", data: data });
+      } else {
+        chrome.runtime.sendMessage({ type: "not_logged_in" });
+      }
     })
     .catch(err => {
       console.error("Error fetching notifications:", err);
     });
 }
 
-chrome.cookies.get({ url: "https://imgflip.com", name: "iflipsess" }, function(cookie) {
-  if (cookie) {
-    fetchNotifications(cookie.value);
-  } else {
-    console.log("You must be logged in to get notifs");
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === "popup_opened") {
+    console.log("popup opened received");
+    fetchData();
   }
 });
+
+fetchData();
 
 const intervalInMinutes = .33;
 const intervalInMilliseconds = intervalInMinutes * 60 * 1000;
 
 setInterval(() => {
-  chrome.cookies.get({ url: "https://imgflip.com", name: "iflipsess" }, function(cookie) {
-    if (cookie) {
-      fetchNotifications(cookie.value);
-    } else {
-      console.log("You must be logged in to get notifs");
-    }
-  });
+  fetchData();
 }, intervalInMilliseconds);
