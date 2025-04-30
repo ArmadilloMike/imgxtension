eed875c325876750382e78219b9d3f07c5932adc
32-Bit <103079138+CreeperKing77@users.noreply.github.com>
2025-04-30 09:52:29 -0500

    Update update-theme.js

--- scripts/update-theme.js
+++ scripts/update-theme.js
@@ -1,27 +1,60 @@
+
 chrome.storage.local.get(['useTheme'], result => {
     const setting = result.useTheme ?? false; 
 
 
     if (typeof window !== "undefined" && window.location.href.includes("imgflip.com") && setting == true) {
-        // Dynamically inject the CSS
-        const cssUrl = chrome.runtime.getURL("themes.css");
-        const link = document.createElement("link");
-        link.href = cssUrl;
-        link.type = "text/css";
-        link.rel = "stylesheet";
-        document.head.appendChild(link);
+        chrome.storage.local.get(['theme'], result => {
+            const theme = result.theme ?? 0;
+
+            const themes = ["default",
+                            "msmg-purple",
+                            "dark-purple",
+                            "orange",
+                            ];
+
+            // Dynamically inject the CSS
+            let cssUrl = chrome.runtime.getURL("themes.css");
+            let link = document.createElement("link");
+            link.href = cssUrl;
+            link.type = "text/css";
+            link.rel = "stylesheet";
+            document.head.appendChild(link);
+
+            // Dynamically inject the CSS
+            cssUrl = chrome.runtime.getURL(`../themes/${themes[theme]}.css`);
+            link = document.createElement("link");
+            link.href = cssUrl;
+            link.type = "text/css";
+            link.rel = "stylesheet";
+            document.head.appendChild(link);
 
 
 
-        const img = document.getElementById("logo");
-        const img_mobile = document.getElementById("logo-icon");
+            const img = document.getElementById("logo");
+            const img_mobile = document.getElementById("logo-icon");
 
-        if (img) {
-            let newSrc = "";
-            newSrc = chrome.runtime.getURL("images/imgflip-hue.png");
-            img.src = newSrc;
-            newSrc = chrome.runtime.getURL("images/imgmob-hue.png");
-            img_mobile.src = newSrc;
-        }
+
+            // Wait for the theme CSS to load
+            link.addEventListener('load', () => {
+                console.log('Theme CSS loaded!');
+
+                const img = document.getElementById("logo");
+                const img_mobile = document.getElementById("logo-icon");
+
+                const msmg_img = document.getElementById("msmg-logo");
+                const msmg_img_mobile = document.getElementById("msmg-logo-icon");
+
+                if (img) {
+                    const style = window.getComputedStyle(document.body);
+                    const titleshift = style.getPropertyValue('--titleshift');
+                    const shift = titleshift ? titleshift.trim() : '0deg';
+                    img.style.filter = `hue-rotate(${shift})`;
+                    img_mobile.style.filter = `hue-rotate(${shift})`;
+
+                }
+            });
+
+        });
     }
 });
