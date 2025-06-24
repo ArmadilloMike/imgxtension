931c2d140493efa43588ad635e440198f91678ec
32-Bit <103079138+CreeperKing77@users.noreply.github.com>
2025-06-24 11:35:20 -0500

    added v number to popup

--- popup/options.php
+++ popup/options.php
@@ -8,6 +8,12 @@
       document.getElementById("settingsBtn").addEventListener("click", function() {
         chrome.runtime.openOptionsPage();  // Opens the options page
       });
+
+      const manifest = chrome.runtime.getManifest();
+      const manifestVersion = manifest.manifest_version;
+      
+      let box = document.querySelector('.user-title')
+      const html = `<br><p>version: ${manifestVersion}</p>`;
     </script>
   </body>
 </html>
